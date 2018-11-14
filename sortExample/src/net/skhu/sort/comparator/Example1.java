7ac5153e26764a30d542a67035d73be7ebba63a2
hyerin6 <0419hyerin@gmail.com>
2018-11-14 19:18:05 +0900

    sortExam

---
+++ sortExample/src/net/skhu/sort/comparator/Example1.java
@@ -0,0 +1,23 @@
+package net.skhu.sort.comparator;
+
+import java.util.Arrays;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Person[] a = new Person[] {
+				new Person("홍길동", 18),
+				new Person("임꺽정", 22),
+				new Person("전우치", 23) 
+		};
+
+
+		Arrays.sort(a, new PersonNameComparator());
+		System.out.println(Arrays.toString(a));
+
+		Arrays.sort(a, new PersonAgeComparator());
+		System.out.println(Arrays.toString(a));
+	}
+}
